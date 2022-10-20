3ac52ae9e3c6c92674d53dc8496c6716a3109bdd
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-10-21 03:25:05 +0800

    Implemented the display of user data in a table in Dashboard

--- group2-proj/src/FetchRecords.js
+++ group2-proj/src/FetchRecords.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 
-const FetchRecords = () => {
+import ViewDetails from './ViewDetails';
 
+const FetchRecords = (props) => {
 	const [records, setRecords] = useState([]);
 	const [totalUsers, setTotalUsers] = useState(0);
+	const [showViewDetails, setViewDetails] = useState(false);
+	let [selectedUser, setSelectedUser] = useState('');
 
 	const fetchData = async () => {
 		const response = await fetch(
@@ -14,27 +17,58 @@ const FetchRecords = () => {
 	  	setTotalUsers(response.length);
 	};
 
+	const handleViewDetails = () => {
+		selectedUser = records.id;
+		setSelectedUser(selectedUser);
+		setViewDetails(true);
+	}
+
 
 	useEffect(()=>{
 		fetchData();	
 	},[]);
 
-	return(
-		records ? (
-			<>
-				<h1>Users</h1>
-				<ul>
-				{ 	records.length && records.map((rec, i)=>(
-						<li key={i} data-testid='user'>{rec.name}</li>
-					))
-				}
-				</ul>
-				<h2 data-testid="total-users">Total Users: {totalUsers}</h2>
-			</>
-		): (
-			<p>Fetching...</p>
-		)		
-	);
+	return !showViewDetails ? (
+		<>
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Username</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ records.length && records.map((records, i)=> (
+						<tr>
+							<td>{records.id}</td>
+							<td>{records.name}</td>
+							<td>{records.username}</td>
+							<td>{records.email}</td>
+							<td>{records.phone}</td>
+							<td><a href="#\" onClick={handleViewDetails}>View Details</a></td>
+						</tr>
+					))}
+				</tbody>
+				
+			</table>
+			{/* <h1>Users</h1>
+			<ul>
+			{ 	records.length && records.map((rec, i)=>(
+					<li key={i} data-testid='user'>{rec.id}</li>
+				))
+			}
+			</ul>
+			<h2 data-testid="total-users">Total Users: {totalUsers}</h2> */}
+			<h2 data-testid="total-users">Total Customers: {totalUsers}</h2>
+		</>
+	): (
+		 <ViewDetails userID={selectedUser}/>
+		// <p>Fetching...</p>
+	)
 };
 
 export default FetchRecords;
